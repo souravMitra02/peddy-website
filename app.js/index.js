ff7a54e6da1b331e90766de91e2aa0167b983ee2
souravMitra02 <inf.souravmitra@gmail.com>
2025-03-31 20:55:48 +0600

    added footer section

--- app.js/index.js
+++ app.js/index.js
@@ -1,24 +1,39 @@
 const loadCategory = async () => {
+  document.getElementById('error').style.display = 'none';
+  loadingHide('data-loading')
     const response = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
     const data = await response.json();
-    displayCategory(data.categories);
+  displayCategory(data.categories);
+  
 }
 
-const loadPets = async(petsName) => {
-    const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${petsName}`);
-    const data = await response.json()
+const loadPets = async (petsName) => {
+  document.getElementById('pets-container').style.display = 'block';
+  document.getElementById('error').style.display = 'none';
+  document.getElementById('pets-container').style.display = 'grid';
+  document.getElementById('pets-container').style.display = 'grid-cols-3';
+  loadingShow('data-loading');
+const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${petsName}`);
+  const data = await response.json()
+  loadingHide('data-loading')
     displayPet(data.data);
 }
 
 const displayPet = (petData) => {
-    const petContainer = document.getElementById('pets-container');
-    petContainer.innerHTML = "";
+
+  if (petData.length === 0) {
+    document.getElementById('pets-container').style.display = 'none';
+    document.getElementById('error').style.display = 'block';
+  }
+  const petsContainer = document.getElementById('pets-container');
+ 
+    petsContainer.innerHTML = "";
     for (const data of petData) {
-        const div = document.createElement('div');
-       
-       
+      const div = document.createElement('div');
+    
+        const petsContainer = document.getElementById('pets-container');
         div.innerHTML = `
-        <div class="border-2 border-[#1313131A] rounded-lg">
+        <div class="border-2 border-[#1313131A] rounded-lg ">
   <figure>
     <img class =" p-5  mx-auto rounded-3xl"
       src="${data.image}" />
@@ -33,16 +48,57 @@ const displayPet = (petData) => {
     <p><i class="fa-solid fa-dollar-sign"></i> Price : ${data.price}$</p>
     <div class="items-center gap-5 flex mb-3">
   <i class="fa-regular fa-thumbs-up fa-xl border-2 rounded-md border-[#0E7A8126] px-6 py-4 hover:cursor-pointer hover:text-white hover:bg-[#0E7A81]"></i>
-      <P class=" font-bold border-2 rounded-md border-[#0E7A8126] px-4 py-1 text-[#0E7A81] hover:cursor-pointer hover:text-white hover:bg-[#0E7A81]">Adopt</P>
-     <p class=" font-bold border-2 rounded-md border-[#0E7A8126] px-4 py-1 text-[#0E7A81] hover:cursor-pointer hover:text-white hover:bg-[#0E7A81]">Details</p>
+      <button class="font-bold border-2 rounded-md border-[#0E7A8126] px-4 py-1 text-[#0E7A81] hover:cursor-pointer hover:text-white hover:bg-[#0E7A81]">Adopt</button>
+     <button onclick = "petDetails(${data.petId})" class=" font-bold border-2 rounded-md border-[#0E7A8126] px-4 py-1 text-[#0E7A81] hover:cursor-pointer hover:text-white hover:bg-[#0E7A81] all-details">Details</button>
     </div>
+    <div>
   </div>
 </div>
-        `
-        petContainer.appendChild(div);
 
-    }
+<div>
+</div>
+        `
+        petsContainer.appendChild(div);
+
+       
+  }
+  
+
+
 }
+
+const petDetails = (petId) => {
+  const url = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
+  fetch(url)
+    .then(res => res.json())
+  .then(data=>displayDetails(data.petData))
+
+}
+
+const displayDetails = (details) => {
+  console.log(details);
+  document.getElementById('pet_details').showModal()
+  const modal = document.getElementById('modal_container');
+  modal.innerHTML = `
+  <div>
+  <img class="w-9/12 mx-auto rounded-xl" src=${details.image}></img>
+  <h2 class="text-2xl font-bold">Pet Name : ${details.pet_name}</h2>
+  <div class="grid grid-cols-2 border-b-2 border-[#1313131A]">
+   <p><i class="fa-solid fa-magnifying-glass"></i> Breed : ${details.breed}</p>
+    <p><i class="fa-regular fa-calendar"></i> Birth : ${details.date_of_birth}</p>
+    <p><i class="fa-solid fa-venus"></i> Gender : ${details.gender}</p>
+    <p><i class="fa-solid fa-dollar-sign"></i> Price : ${details.price}$</p>
+    <p><i class="fa-solid fa-dollar-sign mb-5"></i> Vaccinated status :  ${details.vaccinated_status}</p>
+    </div>
+    <h3 class="font-bold text-xl">Details Information</h3>
+    <p>${details.pet_details}</p>
+  </div>
+  `
+}
+
+
+
+
 
 const displayCategory = (category) => {
     
@@ -62,5 +118,16 @@ const displayCategory = (category) => {
   }
    
 }
+
+const loadingHide = (id) => {
+  
+  document.getElementById(id).style.display = 'none';
+}
+const loadingShow = (id) => {
+  
+  document.getElementById(id).style.display = 'block';
+}
+
+
 
 loadCategory()
